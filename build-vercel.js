30eec895ecbe5293d073de506c18a34b47883e74
Noam Rosenthal <noamr@users.noreply.github.com>
2022-12-01 10:40:31 +0200

    Use vercel output dir

--- build-vercel.js
+++ build-vercel.js
@@ -2,15 +2,13 @@ const {resolve} = require("path");
 const commandLineArgs = require('command-line-args');
 const {copySync, rmSync, mkdirSync} = require("fs-extra");
 const {buildClientBundle, buildVercelMiddleware} = require("./index");
-const { existsSync, writeFileSync } = require("fs");
+const { existsSync, writeFileSync, write, rm, rmdirSync } = require("fs");
 
 const optionDefinition = [
     {name: 'dir', alias: 'd', defaultOption: true},
     {name: 'ssr', alias: 's', type: Boolean},
     {name: 'public', alias: 'u', multiple: true},
     {name: 'minify', alias: 'm', type: Boolean},
-    {name: 'out', alias: 'o'},
-    {name: 'name', alias: 'n'}
 ];
 
 const runOptions = commandLineArgs(optionDefinition, process.argv);
@@ -20,38 +18,42 @@ if (!runOptions.dir?.length) {
     process.exit(0);
 }
 
-const {dir, out} = runOptions;
-
-if (existsSync(out))
-  rmSync(out, {recursive: true});
-mkdirSync(out);
+const {dir, ssr, public, minify} = runOptions;
 
 const indexModule = resolve(dir, "index.h0.ts");
-const publicFolders = [...runOptions?.public || [], resolve(dir, "public")];
+const publicFolders = [...public || [], resolve(dir, "public")];
 const htmlFile = resolve(dir, "template.h0.html");
 if (!existsSync(htmlFile))
     throw new Error(`Template ${htmlFile} not found`);
 if (!existsSync(indexModule))
     throw new Error(`Index ${indexModule} not found`);
 
+if (existsSync(".vercel"))
+  rmSync(".vercel", {recursive: true});
+
+  mkdirSync(".vercel");
+  mkdirSync(".vercel/output");
+  mkdirSync(".vercel/output/_middleware.func");
+  mkdirSync(".vercel/output/static");
+
 for (const pub of publicFolders)
-  copySync(pub, resolve(out, "public"));
+  copySync(pub, ".vercel/output/static");
 
-buildClientBundle(indexModule, resolve(out, "public", ".h0"), {minify: !!runOptions.minify});
-buildVercelMiddleware(dir, resolve(out, "middleware.js"), {serverSideRendering: runOptions.ssr});
 
-writeFileSync(resolve(out, "package.json"), `{
-  "name": "${runOptions.name}",
-  "version": "1.0.0",
-  "description": "",
-  "main": "middleware.js",
-  "scripts": {
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "@vercel/edge": "^0.1.2",
-    "linkedom": "^0.14.21"
-  }
-}
-`);
+  writeFileSync(".vercel/output/config.json", JSON.stringify({
+  "version": 3,
+  "routes": [
+    {
+      "src": "/(.*)",
+      "middlewarePath": "_middleware",
+      "continue": true
+    }
+  ]
+}));
+
+writeFileSync(".vercel/output/_middleware.func/.vc-config.json", JSON.stringify({
+  "runtime": "edge",
+  "entrypoint": "index.js"
+}));
+buildClientBundle(indexModule, resolve(".vercel/output/static/.h0"), {minify: !!minify});
+buildVercelMiddleware(dir, ".vercel/output/_middleware.func/index.js", {serverSideRendering: ssr});
